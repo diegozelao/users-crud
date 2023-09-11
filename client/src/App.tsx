17cb09567cb24e49cab1b8be39aac99ede1fe7aa
Diego David <diegozelao@gmail.com>
2023-09-11 05:06:50 -0300

    chore(tailwind): config tailwind and add imports configs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,7 @@
+import EntryPage from "./pages/EntryPage"
 function App() {
-
   return (
-    <div className="h-screen flex flex-1 bg-zinc-800 text-red-500">
-      aaaaa
-    </div>
+    <EntryPage />
   )
 }
 
---
+++ client/src/pages/EntryPage.tsx
@@ -0,0 +1,9 @@
+
+
+export default function EntryPage() {
+  return (
+    <div className="initBody">
+      <h1>Entry Page</h1>
+    </div>
+  )
+}

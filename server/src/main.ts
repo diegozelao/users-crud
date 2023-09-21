67e833f260ee1990c2904266520188931dc6d2c4
Diego David <diegozelao@gmail.com>
2023-09-21 19:42:04 -0300

    feat(cors): add whiitelist for localhost

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,23 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const whitelist = [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:3002',
+    'http://localhost:8000',
+    'http://127.0.0.1:3000',
+    'http://127.0.0.1:3001',
+    'http://127.0.0.1:3002',
+    // 'http://10.0.2.2:3000',
+  ];
   const cors = {
-    origin: ['http://localhost:3000'],
+    // origin: ['http://localhost:3000'],
+    // origin: '*',
     methods: 'GET, HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: whitelist,
+    allowedHeaders: ['content-type'],
+    credentials: true,
   };
 
   app.enableCors(cors);
